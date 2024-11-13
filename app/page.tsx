826dcc2da5e5506ac4acf8f7e1fdfb860f1c1e1f
abdelmatine <abdelmatinesfar@gmail.com>
2024-11-13 12:53:38 +0100

    finalizing Auth

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,15 @@
+import { redirect } from "next/navigation";
+import { Navbar } from "./components/Navbar";
+import { auth } from "./lib/auth";
 
-
-export default function Home() {
-  return <h1>hello boy</h1>;
+export default async function Home() {
+  const session = await auth();
+  if (session?.user) {
+    return redirect ("/dashboard");
+  }
+  return (
+    <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+      <Navbar />
+    </div>
+  );
 }
