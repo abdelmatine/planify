9bfa1f3736d984feb231cc0b5ade2584a0083916
abdelmatine <abdelmatinesfar@gmail.com>
2024-11-19 19:14:47 +0100

    Availability Impl

--- app/actions.ts
+++ app/actions.ts
@@ -5,6 +5,7 @@ import { requireUser } from "./lib/hooks";
 import {parseWithZod} from '@conform-to/zod'
 import { onboardingSchema, onboardingSchemaValidation, settingsSchema } from "@/app/lib/zodSchemas";
 import { redirect } from "next/navigation";
+import { revalidatePath } from "next/cache";
 
 export async function OnBoardingAction(prevState: any, formData: FormData) {
     const session = await requireUser();
@@ -36,6 +37,47 @@ export async function OnBoardingAction(prevState: any, formData: FormData) {
         data: {
             userName: submission.value.userName,
             name: submission.value.fullName,
+            availability: {
+                createMany: {
+                    data: [
+                        {
+                            day: "Monday",
+                            fromTime: "08:00",
+                            tillTime: "18:00"
+                        },
+                        {
+                            day: "Tuesday",
+                            fromTime: "08:00",
+                            tillTime: "18:00"
+                        },
+                        {
+                            day: "Wednesday",
+                            fromTime: "08:00",
+                            tillTime: "18:00"
+                        },
+                        {
+                            day: "Thursday",
+                            fromTime: "08:00",
+                            tillTime: "18:00"
+                        },
+                        {
+                            day: "Friday",
+                            fromTime: "08:00",
+                            tillTime: "18:00"
+                        },
+                        {
+                            day: "Saturday",
+                            fromTime: "08:00",
+                            tillTime: "18:00"
+                        },
+                        {
+                            day: "Sunday",
+                            fromTime: "08:00",
+                            tillTime: "18:00"
+                        },
+                    ]
+                }
+            }
         }
     });
 
@@ -65,4 +107,39 @@ const user = await prisma.user.update({
 
 return redirect("/dashboard");
 
+}
+
+export async function updateAvailabilityAction(formData: FormData){
+    const session = await requireUser();
+
+    const rawData = Object.fromEntries(formData.entries())
+
+    const availabilityData = Object.keys(rawData).filter((key) => key.startsWith("id-")).map((key) => {
+        const id = key.replace("id-", "");
+
+        return {
+            id,
+            isActive: rawData[`isActive-${id}`] === "on",
+            fromTime: rawData[`fromTime-${id}`] as string,
+            tillTime: rawData[`tillTime-${id}`] as string,
+        };
+    });
+
+    try{
+        await prisma.$transaction(
+            availabilityData.map((item) => prisma.availability.update({
+                where: {
+                    id: item.id,
+                },
+                data: {
+                    isActive: item.isActive,
+                    fromTime: item.fromTime,
+                    tillTime: item.tillTime,
+                },
+            }))
+        );
+        revalidatePath("/dashboard/availability")
+    } catch (error){
+        console.log(error);
+    }
 }
